16723a6df441e8f49395a68a295825df877e78f5
sophittha <smilinglypop@gmail.com>
2015-07-05 14:12:17 +0700

    add signature java_layout

--- app/src/main/java/com/example/palm/synovaexpress/SignatureActivity.java
+++ app/src/main/java/com/example/palm/synovaexpress/SignatureActivity.java
@@ -1,17 +1,14 @@
 package com.example.palm.synovaexpress;
 
-import android.os.Bundle;
-
 import android.app.Activity;
 import android.os.Bundle;
 
 /**
  * Created by paloppop on 7/5/2015.
  */
-public class LoginActivity extends Activity {
+public class SignatureActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
     }
 }
