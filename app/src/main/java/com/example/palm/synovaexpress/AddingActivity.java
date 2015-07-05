904eaeb8714601a9fcccc9d1c83e29d355cc1217
sophittha <smilinglypop@gmail.com>
2015-07-05 22:01:57 +0700

    scan

--- app/src/main/java/com/example/palm/synovaexpress/AddingActivity.java
+++ app/src/main/java/com/example/palm/synovaexpress/AddingActivity.java
@@ -1,15 +1,30 @@
 package com.example.palm.synovaexpress;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 /**
  * Created by paloppop on 7/5/2015.
  */
 public class AddingActivity extends Activity {
+
+    ImageButton scan_btn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adding);
+
+        scan_btn = (ImageButton)findViewById(R.id.scanButton);
+        scan_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(AddingActivity.this,ScanInputActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/palm/synovaexpress/ScanInputActivity.java
+++ app/src/main/java/com/example/palm/synovaexpress/ScanInputActivity.java
@@ -1,14 +1,54 @@
 package com.example.palm.synovaexpress;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.ActivityNotFoundException;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 /**
  * Created by paloppop on 7/5/2015.
  */
 public class ScanInputActivity extends Activity{
+
+    static final String ACTION_SCAN = "com.google.zxing.client.android.SCAN";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        scanBar();
+    }
+
+    public void scanBar() {
+        try {
+            Intent intent = new Intent(ACTION_SCAN);
+            intent.putExtra("SCAN_MODE", "PRODUCT_MODE");
+            startActivityForResult(intent, 0);
+        } catch (ActivityNotFoundException anfe) {
+            Toast toast = Toast.makeText(this,"Catch error:" + anfe.toString(),Toast.LENGTH_LONG);
+            toast.show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+        if (requestCode == 0) {
+            if (resultCode == RESULT_OK) {
+                String contents = intent.getStringExtra("SCAN_RESULT");
+                String format = intent.getStringExtra("SCAN_RESULT_FORMAT");
+
+                Toast toast = Toast.makeText(this, "Content:" + contents + " Format:" + format, Toast.LENGTH_LONG);
+                toast.show();
+
+                Intent link = new Intent(ScanInputActivity.this,AddingStep1Activity.class);
+                link.putExtra("barcode",contents);
+                startActivity(link);
+            }
+        }
     }
 }
