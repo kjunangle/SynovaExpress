df17112795c9e372305ad7c0b26154a379ecd7dd
Burawat Wangwonsin <Palm@Palms-MacBook-Pro.local>
2015-07-05 13:45:24 +0700

    uptest

--- app/src/main/java/com/example/palm/synovaexpress/LoginActivity.java
+++ app/src/main/java/com/example/palm/synovaexpress/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.palm.synovaexpress;
 
+<<<<<<< Updated upstream
 import android.app.Activity;
 import android.os.Bundle;
 
@@ -12,4 +13,10 @@ public class LoginActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
+=======
+/**
+ * Created by Palm on 7/5/15 AD.
+ */
+public class LoginActivity {
+>>>>>>> Stashed changes
 }
