a0f45666bb74cb11124c1058df486db08be4ac9c
sophittha <smilinglypop@gmail.com>
2015-07-06 23:24:04 +0700

    Slide for call

--- app/src/main/java/com/example/palm/synovaexpress/InformationActivity.java
+++ app/src/main/java/com/example/palm/synovaexpress/InformationActivity.java
@@ -5,18 +5,29 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 /**
  * Created by paloppop on 7/5/2015.
  */
-public class InformationActivity extends Activity {
+public class InformationActivity extends Activity implements SeekBar.OnSeekBarChangeListener {
 
     ImageButton scan_btn,pending_btn;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_information);
+
+        textView = (TextView)findViewById(R.id.phone);
+        textView.setText("0831039888");
+
+        SeekBar seekBar = (SeekBar)findViewById(R.id.slider);
+        seekBar.setMax(100);
+        seekBar.setProgress(0);
+        seekBar.setOnSeekBarChangeListener(this);
 
         scan_btn = (ImageButton)findViewById(R.id.scanButton);
         scan_btn.setOnClickListener(new View.OnClickListener() {
@@ -35,5 +46,26 @@ public class InformationActivity extends Activity {
                 startActivity(intent);
             }
         });
+
+
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+        if(i >= 90){
+            Intent intent = new Intent(InformationActivity.this,PhoneActivity.class);
+            intent.putExtra("phoneNumber",textView.getText().toString());
+            startActivity(intent);
+        }
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+
     }
 }
---
+++ app/src/main/java/com/example/palm/synovaexpress/PhoneActivity.java
@@ -0,0 +1,88 @@
+package com.example.palm.synovaexpress;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.telephony.PhoneStateListener;
+import android.telephony.TelephonyManager;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+/**
+ * Created by paloppop on 7/6/2015.
+ */
+public class PhoneActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        String phoneNumS = getIntent().getExtras().getString("phoneNumber");
+        Integer phoneNum = Integer.parseInt(phoneNumS);
+
+        MyPhoneListener phoneListener = new MyPhoneListener();
+        TelephonyManager telephonyManager = (TelephonyManager)this.getSystemService(Context.TELEPHONY_SERVICE);
+        telephonyManager.listen(phoneListener, PhoneStateListener.LISTEN_CALL_STATE);
+
+        try {
+            // set the data
+            //for input telephone number
+            String uri = "tel:0" + phoneNum;
+            Intent callIntent = new Intent(Intent.ACTION_CALL, Uri.parse(uri));
+            startActivity(callIntent);
+        }catch(Exception e) {
+            Toast.makeText(getApplicationContext(), "Your call has failed...",
+                    Toast.LENGTH_LONG).show();
+            e.printStackTrace();
+        }
+    }
+
+    private class MyPhoneListener extends PhoneStateListener {
+        private boolean onCall = false;
+
+        @Override
+        public void onCallStateChanged(int state, String incomingNumber) {
+            super.onCallStateChanged(state, incomingNumber);
+
+            switch (state) {
+                case TelephonyManager.CALL_STATE_RINGING:
+                    // phone ringing...
+                    Toast.makeText(PhoneActivity.this, incomingNumber + " calls you",
+                            Toast.LENGTH_LONG).show();
+                    break;
+
+                case TelephonyManager.CALL_STATE_OFFHOOK:
+                    // one call exists that is dialing, active, or on hold
+                    //Toast.makeText(MainActivity.this, "on call...",Toast.LENGTH_SHORT).show();
+
+                    //because user answers the incoming call
+                    onCall = true;
+                    break;
+
+                case TelephonyManager.CALL_STATE_IDLE:
+                    // in initialization of the class and at the end of phone call
+                    // detect flag from CALL_STATE_OFFHOOK
+                    if (onCall == true) {
+                        //Toast.makeText(MainActivity.this, "restart app after call",Toast.LENGTH_LONG).show();
+
+                        // restart our application
+                        //Intent restart = getBaseContext().getPackageManager().
+                        //        getLaunchIntentForPackage(getBaseContext().getPackageName());
+
+                        Intent intent = new Intent(PhoneActivity.this,InformationActivity.class);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        startActivity(intent);
+                        onCall = false;
+                    }
+                    break;
+
+                default:
+                    break;
+            }
+
+        }
+    }
+}
